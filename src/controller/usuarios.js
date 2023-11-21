250877af8076424a57193fd9fd6a345d4b01f312
paula borges <paulabzambeli@gmail.com>
2023-11-21 16:03:08 -0300

    middleware

--- src/controller/usuarios.js
+++ src/controller/usuarios.js
@@ -1,13 +1,25 @@
+const bcrypt = require('bcrypt');
 const pool = require('../database/conexao')
+const jwt = require('jsonwebtoken');
+const senhaJwt = require('../senhaJwt');
 
 const signUp = async (req,res) => {
 
   const {nome, email, senha, telefone} = req.body;
   try {  
+    const senhaCriptografada = await bcrypt.hash(senha,10)
+        
+    const buscarEmail = await pool.query(
+    "select * from usuarios where email =$1",
+    [email]
+    )
+    if (buscarEmail.rowCount > 0) {
+      return res.status(401).json({ mensagem: "E-mail já existente" });
+    }
 
     const novoUsario = await pool.query(
-      "insert into usuarios (nome, email, senha, telefone) values ($1,$2,$3,$4) returning*",
-      [nome, email, senha, telefone]
+    "insert into usuarios (nome, email, senha, telefone) values ($1,$2,$3,$4) returning*",
+    [nome, email, senhaCriptografada, telefone]
     )
 
     return res.status(201).json(novoUsario.rows[0]);
@@ -17,6 +29,62 @@ const signUp = async (req,res) => {
     }
 }
 
+const login = async (req, res) => { 
+   const { email, senha } = req.body;
+   try {
+    
+    if (!email || !senha) {
+      return res.status(401).json({ mensagem: "Todos os campos são obrigatórios" });
+    }
+
+   const procurarUsuario = await pool.query(
+    "select * from usuarios where email = $1",
+    [email]
+    );
+
+    if (!procurarUsuario.rowCount ) {
+      return res.status(401).json({ messagem: "Email e/ou senha inválidos" });
+        } 
+  
+
+    const senhaValida = await bcrypt.compare(senha, procurarUsuario.rows[0].senha);
+
+    if (!senhaValida) {
+      return res.status(401).json({ menssagem: "Email e/ou senha inválidos" });
+      }
+    
+   const { senha: _, ...usuarioLogado } = procurarUsuario.rows[0];
+
+    
+   const token = jwt.sign({ id: usuarioLogado.id }, senhaJwt, { expiresIn: "1h", });
+    
+    return res.status(200).json({ usuario: usuarioLogado, token});
+
+      } catch (error) {
+        return res.status(500).json(error.message);
+      }
+  }
+  const buscarUsuario = async (req, res) => {
+    const { id } = req.usuario;
+    try {
+      const obterUsuario = await pool.query(
+        "select * from usuarios where id = $1 ",
+        [id]
+      );
+  
+      if (obterUsuario < 1) {
+        return res.status(404).json({ mensagem: "Usuário não encontrado" });
+      }
+  
+      return res.status(200).json(obterUsuario.rows[0]);
+
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  };
+
 module.exports ={
   signUp,
+  login,
+  buscarUsuario
 }
--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,10 +1,20 @@
 const express = require('express');
 
-const {signUp} = require('../controller/usuarios');
+const {
+    signUp,
+    login,
+    buscarUsuario,
+} = require('../controller/usuarios');
+
+const verificaLogin = require('../middleware/verificaLogin')
 
 const rotas = express();
 
 rotas.post('/usuario', signUp)
+rotas.post('/login',login)
 
+rotas.use(verificaLogin)
+
+rotas.get('/usuario',buscarUsuario)
 
 module.exports = rotas
